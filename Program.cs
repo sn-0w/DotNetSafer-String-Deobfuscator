e47a7ed4ac57f5fd3987b3b97bfbfb22db60fb68
Snow <snowasdev@gmail.com>
2021-11-06 21:41:48 +0100

    Updated Code with another sample test

--- Program.cs
+++ Program.cs
@@ -28,27 +28,39 @@ namespace DotNetSafer_StringDeobfuscator
 
             foreach (var type in md.GetTypes())
             {
-                if (type == md.GlobalType) continue;
                 foreach (var method in type.Methods)
                 {
                     if (!method.HasBody || method.Body == null) continue;
-                    for (int i = 0; i < method.Body.Instructions.Count(); i++)
+                    CallUnaligned(method);
+                    var instr = method.Body.Instructions;
+                    for (var i = 0; i < instr.Count; i++)
                     {
-                        var instr = method.Body.Instructions;
+                        if (instr[i].OpCode != OpCodes.Call)
+                            continue;
 
-                        if (instr[i].OpCode == OpCodes.Br_S && instr[i].Operand == instr[i + 2] && instr[i + 1].OpCode == OpCodes.Unaligned)
-                        {
-                            instr[i].OpCode = OpCodes.Nop;
-                            instr[i + 1].OpCode = OpCodes.Nop;
-                        }
+                        if (!instr[i].Operand.ToString().Contains("System.Convert::ToInt32"))
+                            continue;
+
+                        if (!(instr[i - 2].OpCode == OpCodes.Ldstr) || !(instr[i - 1].IsLdcI4()))
+                            continue;
+
+                        instr[i] = Instruction.CreateLdcI4(Convert.ToInt32(instr[i-2].Operand.ToString(), instr[i - 1].GetLdcI4Value()));
+                        instr[i - 2].OpCode = OpCodes.Nop;
+                        instr[i - 1].OpCode = OpCodes.Nop;
+
 
                     }
                 }
             }
-            ModuleWriterOptions opts = new ModuleWriterOptions(md);
-            opts.MetadataOptions.Flags = MetadataFlags.KeepOldMaxStack;
-            md.Write(args[0].Replace(".exe", "-ignore.exe"), opts);
 
+            var opts = new ModuleWriterOptions(md)
+            {
+                MetadataOptions = { Flags = MetadataFlags.PreserveAll },
+                Logger = DummyLogger.NoThrowInstance
+            };
+            md.Write(args[0].Replace(".exe", "-ignore.exe"), opts);
+            Console.ReadLine();
+           // return;
             // Part 2
             md = ModuleDefMD.Load(args[0].Replace(".exe", "-ignore.exe"));
             Assembly ass = Assembly.LoadFrom(args[0].Replace(".exe", "-ignore.exe"));
@@ -64,6 +76,7 @@ namespace DotNetSafer_StringDeobfuscator
                     {
                         var instr = method.Body.Instructions;
 
+                        if (i >= method.Body.Instructions.Count() - 2) break;
                         if (instr[i].OpCode == OpCodes.Br_S && instr[i].Operand == instr[i + 2] && instr[i + 1].OpCode == OpCodes.Unaligned)
                         {
                             instr[i].OpCode = OpCodes.Nop;
@@ -77,7 +90,7 @@ namespace DotNetSafer_StringDeobfuscator
                             {
 
                                 var local = method.Body.Variables.First();
-                                string firsttext ="";
+                                string firsttext = "";
                                 int amogus = i;
                                 List<object> arguments = new List<object>();
                                 while (amogus >= 0)
@@ -101,7 +114,7 @@ namespace DotNetSafer_StringDeobfuscator
 
                                     if (instr[amogus].IsLdcI4() && instr[amogus - 2].OpCode == OpCodes.Dup && instr[amogus + 1].Operand.ToString().Contains("Boolean"))
                                     {
-                                        arguments.Add((instr[amogus].GetLdcI4Value() == 0) ? false : true );
+                                        arguments.Add((instr[amogus].GetLdcI4Value() == 0) ? false : true);
                                     }
 
                                     if (instr[amogus].OpCode == OpCodes.Newarr)
@@ -111,7 +124,7 @@ namespace DotNetSafer_StringDeobfuscator
                                         break;
                                     }
                                 }
-                                int counter = amogus-4;
+                                int counter = amogus - 4;
                                 while (counter < i)
                                 {
                                     counter++;
@@ -123,21 +136,108 @@ namespace DotNetSafer_StringDeobfuscator
                                 }
                                 instr[counter].OpCode = OpCodes.Nop;
                                 var invokemeth = ass.GetModules().First().ResolveMethod(operandmeth.MDToken.ToInt32());
-                                instr[amogus - 4].Operand = invokemeth.Invoke(null,new object[] {firsttext, arguments.ToArray() }).ToString();
-                                
+                                instr[amogus - 4].Operand = invokemeth.Invoke(null, new object[] { firsttext, arguments.ToArray() }).ToString();
+
                             }
                         }
 
-                        
+
                     }
                 }
             }
             hell.fixcflow(md);
+
+            int count = 0;
+            foreach (var type in md.GetTypes())
+            {
+                type.Name = "type" + count;
+                count++;
+                int count2 = 0;
+                foreach(var method in type.Methods)
+                {
+                    if (method.IsSpecialName) continue;
+                    method.Name = "method" + count2;
+                    count2++;
+                }
+                count2 = 0;
+                foreach (var method in type.Fields)
+                {
+                    method.Name = "field" + count2;
+                    count2++;
+                }
+            }
+
             md.Write(args[0].Replace(".exe", "-deob.exe"), opts);
             Console.WriteLine("done!");
             Console.ReadLine();
         }
 
-        
+
+        // thanks NotPrab https://github.com/NotPrab/AntiDecompiler-Cleaner/blob/master/AntiDecompilerUtils.cs
+        internal static bool DetectCallUnaligned(MethodDef method)
+        {
+            var instr = method.Body.Instructions;
+            for (var i = 0; i < instr.Count; i++)
+            {
+                if (!instr[i].IsBr())
+                    continue;
+                if (instr[i + 1].OpCode.Code != Code.Unaligned)
+                    continue;
+                return true;
+            }
+
+            return false;
+        }
+
+        internal static bool DetectCallConstrained(MethodDef method)
+        {
+            var instr = method.Body.Instructions;
+            for (var i = 0; i < instr.Count; i++)
+                if (instr[i].IsBr() &&
+                    instr[i + 1].OpCode == OpCodes.Constrained)
+                    return true;
+
+            return false;
+        }
+
+
+        internal static bool CallConstrained(MethodDef method)
+        {
+            var hasprotection = DetectCallConstrained(method);
+            if (!hasprotection)
+                return false;
+
+            var instr = method.Body.Instructions;
+            for (var i = 0; i < instr.Count; i++)
+            {
+                if (instr[i].IsBr() && instr[i + 1].OpCode == OpCodes.Constrained)
+                {
+                    Console.WriteLine(instr[i + 1].OpCode + "|" + i + "|" + method.FullName);
+                    instr.RemoveAt(i + 1);
+                }
+            }
+            return true;
+        }
+
+        internal static bool CallUnaligned(MethodDef method)
+        {
+            var hasprotection = DetectCallUnaligned(method);
+            if (!hasprotection)
+                return false;
+
+            var instr = method.Body.Instructions;
+            for (var i = 0; i < instr.Count; i++)
+            {
+                if (!instr[i].IsBr())
+                    continue;
+
+                if (instr[i + 1].OpCode.Code != Code.Unaligned)
+                    continue;
+
+                Console.WriteLine(instr[i + 1].OpCode + "|" + i + "|" + method.Name);
+                instr[i + 1].OpCode = OpCodes.Nop;
+            }
+            return true;
+        }
     }
 }
